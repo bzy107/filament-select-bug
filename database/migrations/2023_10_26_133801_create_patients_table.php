e8d88c9a48bec11c96d16585e5f9846f0fe0f19a
bzy107 <bzy107@gmail.com>
2024-02-09 00:09:33 +0900

    softdelete

--- database/migrations/2023_10_26_133801_create_patients_table.php
+++ database/migrations/2023_10_26_133801_create_patients_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('name')->unique();
             $table->string('type');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
