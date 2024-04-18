99e6bfebcb839956cbf053943c2de4593a59beeb
bzy107 <bzy107>
2024-04-18 21:25:36 +0900

    group togglebutton

--- app/Filament/Resources/PatientResource.php
+++ app/Filament/Resources/PatientResource.php
@@ -40,6 +40,7 @@ class PatientResource extends Resource
                     ->required()
                     ->boolean()
                     ->inline()
+                    ->grouped()
             ]);
     }
 
